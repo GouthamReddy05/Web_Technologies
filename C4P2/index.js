3766a66755e617c52c0b7dde595814deddd107d2
GouthamReddy05 <23311a05z2@cse.sreenidhi.edu.in>
2026-03-26 15:16:14 +0530

    initial commit

--- C4P2/index.js
+++ C4P2/index.js
@@ -7,6 +7,6 @@ app.use(bodyParser.json());
 const router = require('./route/bookroute');
 app.use('/books', router);
 
-app.listen(3001, () => {
-  console.log('Server is running on port 3001');
+app.listen(3002, () => {
+  console.log('Server is running on port 3002');
 });
